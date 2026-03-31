9ee82a91b0b57222ded0a0d8d54b3210368b7f2b
Filip <115399354+alfacentaur1@users.noreply.github.com>
2026-03-31 21:42:24 +0200

    go tests

--- go-cicd-project/cmd/api/main.go
+++ go-cicd-project/cmd/api/main.go
@@ -3,30 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"errors"
-	"encoding/json"
+	"github.com/alfacentaur1/go_backend/internal/handler"
 )
 
 func main() {
 	router := http.NewServeMux()
 	//handle hello endpoint
 	//w - response writer, r - request
-	router.HandleFunc("GET /api/v1/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
-	})
+	router.HandleFunc("GET /api/v1/hello", handler.HelloHandler)
 
-	router.HandleFunc("GET /api/v1/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
-	})
+	router.HandleFunc("GET /api/v1/health", handler.HealthHandler)
 
 	//listen on port 8080
 	fmt.Println("Server is running on port 8080")
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		err = errors.New("failed to start server: " + err.Error())
-		fmt.Println(err)
-	}
+	http.ListenAndServe(":8080", router)
+	
 }
 
